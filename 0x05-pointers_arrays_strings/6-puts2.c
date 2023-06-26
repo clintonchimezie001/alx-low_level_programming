4c66df246862a3f807c6b2a674c80f5f77e8672e
clintonchimezie001 <clinton.chimezie@gmail.com>
2023-06-26 15:20:19 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,7 @@
 
 /**
  * puts2 - prints every other character of a string
- * @str: The string too be treated
+ * @str: The string to be treated
  * Return: void
  */
 
@@ -13,14 +13,14 @@ void puts2(char *str)
 	int i;
 	int j = 0;
 
-	while (str[j] i= '\0')
+	while (str[j] != '\0')
 	{
 	j++;
 	}
 
-	for (i = 0; 1 < j; 1 += 2)
+	for (i = 0; i < j; i += 2)
 	{
-	_putchar(str[1]);
+	_putchar(str[i]);
 	}
 	_putchar('\n');
 }
