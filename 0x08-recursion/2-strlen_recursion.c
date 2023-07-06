a7c578c454b37a1345537f25ed05d994239cb2d1
clintonchimezie001 <clinton.chimezie@gmail.com>
2023-07-06 08:22:26 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,11 +8,11 @@
  */
 int _strlen_recursion(char *s)
 {
-    if (*s == '\0')
-    {
-        return (0);
-    }
+if (*s == '\0')
+{
+return (0);
+}
 
-    s++;
-    return (_strlen_recursion(s) + 1);
+s++;
+return (_strlen_recursion(s) + 1);
 }
