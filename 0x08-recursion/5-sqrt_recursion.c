7ff10b79797aabf72cbeb1387f1cba6472138ccd
clintonchimezie001 <clinton.chimezie@gmail.com>
2023-07-06 07:13:17 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,7 +11,7 @@ int int_sqrt(int n, int i);
  */
 int _sqrt_recursion(int n)
 {
-        return (int_sqrt(n, 1));
+    return (int_sqrt(n, 1));
 }
 
 /**
@@ -23,13 +23,13 @@ int _sqrt_recursion(int n)
  */
 int int_sqrt(int n, int i)
 {
-        int sqrt = i * i;
+    int sqrt = i * i;
 
-        if (sqrt > n)
-                return (-1);
+    if (sqrt > n)
+        return (-1);
 
-        if (sqrt == n)
-                return (i);
+    if (sqrt == n)
+        return (i);
 
-        return (int_sqrt(n, i + 1));
+    return (int_sqrt(n, i + 1));
 }
