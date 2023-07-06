130bc7f14a11f180e9307c8a08dd3232b69d7fa8
clintonchimezie001 <clinton.chimezie@gmail.com>
2023-07-06 08:01:36 -0700

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,13 +4,13 @@
  * factorial - calculate the factorial of a number
  * @n: the number to calculate the factorial
  *
- * Return; INTEGER Vlue
+ * Return: INTEGER Value
  */
-int factorail(int n)
-[
-	if (n < 0)
-		return (-1);
-	if (n <= 1)
-		return (1);
-	return (n * factorial(n-1));
+int factorial(int n)
+{
+        if (n < 0)
+                return (-1);
+        if (n <= 1)
+                return (1);
+        return (n * factorial(n-1));
 }
